cd01c52de61a6113153333ced0a49d71818618e1
GastlyVirus <gstnedgrdrch93@gmail.com>
2023-10-03 18:08:52 -0300

    Add ProductDetail

--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,8 +1,13 @@
+import './style.css'
+
 function ProductDetail() {
     return (
-        <div>
-            ProductDetail
-        </div>
+        <aside className='product-detail flex flex-col fixed right-0 border border-black rounded bg-white'>
+            <div className='flex justify-between items-center p-6'>
+                <h2 className='font-medium text-xl'> Detail </h2>
+
+            </div>
+        </aside>
     )
 }
 
